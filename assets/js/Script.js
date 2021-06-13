d514fa8b17b3ceda9ef0b1ea8f9658f82021964d
Gérard <g.masakidi@outlook.fr>
2021-06-13 20:29:32 +0200

    Ajout du JS + modifications du CV

---
+++ assets/js/Script.js
@@ -0,0 +1,89 @@
+const sr =  ScrollReveal();
+
+// Title animation
+sr.reveal('#t1', {
+    origin: 'bottom' ,
+    distance: '300px' ,
+    duration:1000,
+    reset: true
+});
+
+sr.reveal('#p1', {
+    origin: 'bottom' ,
+    distance: '300px' ,
+    delay: 500,
+    duration:1000,
+    reset: true
+});
+
+// Timeline animation
+if (window.innerWidth < 600) {
+    sr.reveal('.timeline2 ul li div', {
+        origin: 'bottom' ,
+        distance: '200px' ,
+        duration:1000,
+        reset: true
+    });
+    sr.reveal('.timeline2 ul li:nth-child(even) div', {
+        origin: 'bottom' ,
+        distance: '200px' ,
+        duration:1000,
+        reset: true
+    });
+} else {
+    sr.reveal('.timeline2 ul li div', {
+        origin: 'right' ,
+        distance: '200px' ,
+        duration:1000,
+        reset: true
+    });
+    sr.reveal('.timeline2 ul li:nth-child(even) div', {
+        origin: 'left' ,
+        distance: '200px' ,
+        duration:1000,
+        reset: true
+    });
+};
+
+sr.reveal('.timeline2 ul li:nth-child(even) .time', {
+    origin: 'right' ,
+    distance: '200px' ,
+    duration:1000,
+    reset: true
+});
+
+sr.reveal('.timeline2 ul li:nth-child(odd) .time', {
+    origin: 'left' ,
+    distance: '200px' ,
+    duration:1000,
+    reset: true
+});
+
+// Skill animation
+if (window.innerWidth > 600) {
+    sr.reveal('.firstRow', {
+        origin: 'left' ,
+        distance: '200px' ,
+        duration:1000,
+        reset: true
+    });
+    sr.reveal('.secondRow', {
+        origin: 'right' ,
+        distance: '200px' ,
+        duration:1000,
+        reset: true
+    });
+};
+ 
+
+// var svg = document.querySelector('svg');
+
+// function isElementInViewport(el) {
+//   var rect = el.getBoundingClientRect();
+//   var isOutside = (rect.top >= window.innerHeight) || (rect.bottom <= 0);
+//   return !isOutside;
+// }
+
+// window.addEventListener('scroll', function(e) {
+//   svg.classList.toggle('in-view', isElementInViewport(svg));
+// });
